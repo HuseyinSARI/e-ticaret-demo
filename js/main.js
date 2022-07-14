88e042d50d97c505d9289b66a3c3ccba0b88f219
HuseyinSARI <huseyin-sari61@hotmail.com>
2022-07-14 15:44:20 +0300

    vertical navbar öncesi
    dikey navbar öncesi

--- js/main.js
+++ js/main.js
@@ -8,20 +8,22 @@ const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
 const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl));
 
 dropdownList.forEach(d => {
-    if (d._element.classList.contains('huseyin-drop')) {
+    
+    // if (d._element.classList.contains('huseyin-drop')) {
+    //    
+    // }
         d._element.addEventListener('hide.bs.dropdown', event => {
-            event.preventDefault();
-            const menu = d._menu;
-            menu.addEventListener('transitionend', te);
+        event.preventDefault();
+        const menu = d._menu;
+        menu.addEventListener('transitionend', te);
 
-            function te() {
-                d._element.classList.remove('show')
-                menu.classList.remove('show')
-                menu.classList.remove('drop-out');
-                menu.removeAttribute('style');
-                menu.removeEventListener('transitionend', te);
-            }
-            menu.classList.add('drop-out')
-        })
-    }
+        function te() {
+            d._element.classList.remove('show')
+            menu.classList.remove('show')
+            menu.classList.remove('drop-out');
+            menu.removeAttribute('style');
+            menu.removeEventListener('transitionend', te);
+        }
+        menu.classList.add('drop-out')
+    })
 })
