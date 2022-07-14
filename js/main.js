39c091b52f793606d059d90ffffd57b10b47ee53
HuseyinSARI <huseyin-sari61@hotmail.com>
2022-07-14 11:56:48 +0300

    header top side
    header'ın üst yarısı bitti

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
+const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl));
+
+dropdownList.forEach(d => {
+    if (d._element.classList.contains('huseyin-drop')) {
+        d._element.addEventListener('hide.bs.dropdown', event => {
+            event.preventDefault();
+            const menu = d._menu;
+            menu.addEventListener('transitionend', te);
+
+            function te() {
+                d._element.classList.remove('show')
+                menu.classList.remove('show')
+                menu.classList.remove('drop-out');
+                menu.removeAttribute('style');
+                menu.removeEventListener('transitionend', te);
+            }
+            menu.classList.add('drop-out')
+        })
+    }
+})
